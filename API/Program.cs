f82030a497d27b48a27dc2243ca128b769ff8eb6
Bui Tien <tien0206166@huce.edu.vn>
2024-03-23 16:40:48 +0700

    End 7

--- API/Program.cs
+++ API/Program.cs
@@ -2,7 +2,10 @@ using API.Data;
 using Microsoft.EntityFrameworkCore;using System.Text;
 using API.Extensions;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;var builder = WebApplication.CreateBuilder(args);
+using Microsoft.IdentityModel.Tokens;
+using API.Errors;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -13,7 +16,7 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
     .WithOrigins("https://localhost:4200"));
 app.UseAuthentication();
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -21,10 +21,8 @@ export class NavComponent implements OnInit {
   }
 
   login() {
-    this.accountService.login(this.model).subscribe({
-
-      next: _ => this.router.navigateByUrl('/members'),
-      error: error => this.toastr.error(error.error)
+    this.accountService.login(this.model).subscribe({ 
+    next: _ => this.router.navigateByUrl('/members')
     })
   }
 
